c5faeff9bba1d5ff69793c31101253322ab5d463
sami-thedev <saamikhan111222@gmail.com>
2021-12-07 15:59:09 +0500

    first commit

---
+++ public/login page/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "linear-gradient(to right bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('../public/images/sch2.jpg')",
+     },
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "linear-gradient(to right bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('../public/images/school.jpg')",
+     },
+    
+      colors:{
+        new:'#0005',
+          primary:'#FF6363',
+          foot:'#333',
+          
+      }
+
+      
+        
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
